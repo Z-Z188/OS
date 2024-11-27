8681aa9ce3d9dbf058279e2b722c5978cc8b7cf1
xyz <2537464910@qq.com>
2024-11-27 22:09:01 +0800

    modify exp1_signal, add more types of signal

--- exp1/exp1/exp1_signal.cpp
+++ exp1/exp1/exp1_signal.cpp
@@ -3,14 +3,25 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void handle_sigint(int sig) 
-{ 
-    printf("Caught signal %d\n", sig); 
-} 
-  
+void handle_sig(int sig){
+    printf("Caught signal %d\n", sig);
+    if(sig == SIGALRM){
+        alarm(2);
+    }
+}
+
 int main() 
 { 
-    signal(SIGINT, handle_sigint); 
-    while (1) ; 
+    signal(SIGINT, handle_sig);     // ctrl + c
+    signal(SIGUSR1, handle_sig);    // a
+    signal(SIGQUIT, handle_sig);    /* ctrl + \ */
+    signal(SIGALRM, handle_sig);    // alarm
+    alarm(2);
+    while (1){
+        char a = getchar();
+        if(a == 'a'){
+            kill(getpid(), SIGUSR1);
+        }
+    }
     return 0; 
 } 
